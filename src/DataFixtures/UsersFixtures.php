7f9a39711dbc56bac1f18ed74d5a480829e5151f
arttraveller <alx-chz@yandex.ru>
2020-11-05 18:10:01 +0300

    Improved fixtures

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -3,13 +3,25 @@
 namespace App\DataFixtures;
 
 use App\Core\Entities\User\User;
+use App\Core\Services\Auth\PasswordHasher;
+use App\Core\Services\Auth\Tokenizer;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class UsersFixtures extends Fixture
 {
     public const CONFIRMED_USER_EMAIL = 'user@example.com';
+    public const CONFIRMED_USER_PASSwORD = 'password';
 
+    private PasswordHasher $passwordHasher;
+    private Tokenizer $tokenizer;
+
+
+    public function __construct(PasswordHasher $hasher, Tokenizer $tokenizer)
+    {
+        $this->passwordHasher = $hasher;
+        $this->tokenizer = $tokenizer;
+    }
 
     public function load(ObjectManager $manager)
     {
@@ -23,8 +35,8 @@ class UsersFixtures extends Fixture
     {
         $user = User::registerByEmail(
             self::CONFIRMED_USER_EMAIL,
-            'todo_hash',
-            'todo_token',
+            $this->passwordHasher->hash(self::CONFIRMED_USER_PASSwORD),
+            $this->tokenizer->generateConfirmToken()
         );
         $user->confirmRegistration();
 
